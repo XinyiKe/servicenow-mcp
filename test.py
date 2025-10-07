618cea9186e68e18167402b4f67ccbd42159145f
XinyiKe <66403682+XinyiKe@users.noreply.github.com>
2025-10-08 07:45:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import requests, base64
 
-username = "dev2APIaccount"
-password = "US7X1+g@QD*ft#96TLdbik$"
 
 auth_value = base64.b64encode(f"{username}:{password}".encode()).decode()
 headers = {
@@ -10,6 +8,5 @@ headers = {
     "Content-Type": "application/json"
 }
 
-url = "https://illuminadev2.service-now.com/api/now/table/incident"
 resp = requests.get(url, headers=headers, params={"sysparm_limit": 1})
 print(resp.status_code, resp.text)
